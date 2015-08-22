18617986eb37eef642ac8c48a2a2c0442cdd1650
Andreas <andre.andreou@ymail.com>
2015-08-22 13:52:45 +0100

    Added the logger to the entry class as well. Modified the behavior for when reading the file. Didn't notice the optional thing before. Also changed my logic to split the line fully. Before I gave it flexibility by limiting the splits to the number of tokens we expect. The reasoning is that if someone put more tokens, then it would wrong. Since the last token is always a number then an extra token appended to that would still cause an error since it couldn't convert it to a Big Decimal and it saved on the splits (i.e. if someone pasted a really huge line). However like this it hides the actual fault happening. So to also limit the splits, I limit it to just once more than what is allowed...

--- src/main/java/com/gamesys/consoleroulette/application/ConsoleRoulette.java
+++ src/main/java/com/gamesys/consoleroulette/application/ConsoleRoulette.java
@@ -8,6 +8,9 @@ import java.math.BigDecimal;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Class that contains entry point. Sets up the threads, reads the file with the player details and starts the main
  * application/game.
@@ -17,6 +20,9 @@ import java.util.regex.Pattern;
  */
 public final class ConsoleRoulette
 {
+	// logger, because system.out and system.err... meh
+	private static final Logger log = LoggerFactory.getLogger(ConsoleRoulette.class);	
+		
 	private final String PLAYER_RECORD = "player_data.txt";
 	
 	private final int LOWER_BOUND = 1;
@@ -24,6 +30,8 @@ public final class ConsoleRoulette
 	private final int UPPER_BOUND = 36;
 	
 	private final int FRQUENCY = 30;
+	
+	private final int MAX_TOKENS = 3;
 
 	public static void main(String[] args)
 	{
@@ -41,52 +49,67 @@ public final class ConsoleRoulette
 			// Reading the file and creating the players.
 			String line;
 			String[] playerDetails;
-			int tokens = 3;
 			Pattern pattern = Pattern.compile(",");
 			
 			// Variables for the details of the player
-			String userName;
-			BigDecimal totalWin;
-			BigDecimal totalBet;
+			String userName = null;
+			BigDecimal totalWin = BigDecimal.ZERO;
+			BigDecimal totalBet = BigDecimal.ZERO;
 			
 			while ((line = bufferedReader.readLine()) != null)
 			{
-				playerDetails = pattern.split(line, tokens);
+				playerDetails = pattern.split(line, cr.MAX_TOKENS + 1);
 				
 				/*
 				 * While we limit the number of tokens to the expected number we also check if the tokens are less and
 				 * print an error message to the user so he can correct his input.
 				 */
-				if (playerDetails.length < tokens)
+				if (playerDetails.length > cr.MAX_TOKENS)
 				{
-					System.out.println("'" +line + "' was malformed. The format is 'Username TotalWin TotalBet'.");
+					log.warn("'" +line + "' was malformed. The format is 'Username [TotalWin] [TotalBet]'.");
 					continue; // no point continuing, read the next input.
 				}
 				
+				// The user name will always be present.
+				userName = playerDetails[0];
+				
 				try
 				{
-					userName = playerDetails[0];
-					
-					totalWin = new BigDecimal(playerDetails[1]);
-					
-					totalBet = new BigDecimal(playerDetails[2]);
+					if ( playerDetails.length == 3)
+					{
+						totalWin = new BigDecimal(playerDetails[1]);
+						
+						totalBet = new BigDecimal(playerDetails[2]);
+					}
+					else if ( playerDetails.length == 2)
+					{
+						totalWin = new BigDecimal(playerDetails[1]);
+					}					
 				}
 				catch (NumberFormatException nfe)
 				{
-					System.err.println(nfe.getMessage());
-					System.err.println("The bet and win amount must be a real numbers (i.e. 0.15, 3.50)");
+					log.error(nfe.getMessage());
+					log.warn("The total bet and total win amount must be a real numbers (i.e. 0.15, 3.50)");
 					continue; // no point continuing, read the next input.
 				}
 				
 				players.put(userName, new Player(userName, totalBet, totalWin));
 			}
+			
 			bufferedReader.close();
 		}
 		catch (IOException e)
 		{
-			e.printStackTrace();
+			log.error(e.getMessage());
+			log.warn("There was a problem with reading the input.");
 		}
 		
+		// If the file was empty then exit. No game can be played.
+		if (players.isEmpty())
+		{
+			log.info("The file was empty or did not have any valid entries. Exiting...");
+			return ;
+		}
 		// Setting the players in the bet monitor.
 		GameBetMonitor betMonitor = new GameBetMonitor(cr.LOWER_BOUND, cr.UPPER_BOUND, players);
 				
